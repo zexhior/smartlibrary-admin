4eab396c30d624fe530b036d8541eec649dfac6f
zexhior <herizoras1@gmail.com>
2023-03-28 14:11:36 +0300

    [func]:creation de la component Stars pour genere le nombre d'etoile

---
+++ src/widgets/stars/stars.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+interface StarsProps{
+    star: number;
+}
+
+const Stars : React.FC<StarsProps>= ({star})=>{
+    const all_star = [];
+    for(let i = 0; i < 5; i++){
+        if(i<star){
+            all_star.push(<FaStar fill="#DECC16"/>);
+        }
+        else{
+            all_star.push(<FaStar fill="#B6A58A"/>)
+        }
+    } 
+
+
+    return (<div className="stars">
+        {
+            all_star.map((element,i)=>{
+                return element;
+            })
+        }
+    </div>)
+}
+
+export default Stars;
